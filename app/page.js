3ef6d9d6cfd680c49c402c1e9bab11819a3d98ee
dheerajup98 <dheerajup98@gmail.com>
2024-09-26 13:04:32 +0530

    completed

--- app/page.js
+++ app/page.js
@@ -1,15 +1,18 @@
 
 import React from "react";
-import AddNewInterview from "./dashboard/_components/AddNewInterview";
+
 import Header from "./dashboard/_components/Header";
 
 
 export default function Home() {
   return (
-    <div>
+    
+   <div>
       <Header/>
         <h1 className="text-5xl pt-5 flex justify-center align-middle">AI Take Mock Interview</h1>
      </div>
+    
+ 
    
     
   
